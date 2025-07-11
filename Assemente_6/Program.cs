0c2e8f89b8f0f9d71c647eafc9f15e09327c5adf
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-11 21:06:22 +0300

    Add project files.

---
+++ Assemente_6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assemente_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region V-1
+            
+            #endregion
+        }
+    }
+}
